9a371e0b8841aa912968e01ffbf7ce1cb67d7cba
Maryamabid142 <za3616161@gmail.com>
2024-11-13 09:51:53 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+document.getElementById("a1").innerHTML="selection"
+document.getElementById("a1").style.color="blue";
+
+
+document.getElementById("a2").style.fontSize="40px"
+document.getElementById("a1").style.paddingLeft="40px";
+document.getElementById("a3").innerHTML="z";
+document.getElementById("a3").innerHTML=
++ z + ".";
+
+
+
+
+
